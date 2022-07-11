605a093959633bec22898dce31ad299cfddc73cc
João Amador <nnettoolliveira@gmail.com>
2022-07-11 02:19:05 -0300

    :wrench: Atualizando configurações inicial no index
    Atualizando e adiconando novos arquivos

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const session = require('express-session')
-const FileStore = require('session-file-store')
+const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
 
 const app = express()
 
 const conn = require('./db/conn')
+
+// template engine 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// receber resposta do body
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+// session middleware
+app.use(
+  session({
+    name: 'session',
+    secrete: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () { },
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true
+    }
+  })
+)
+
+// flash messages
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+// set session to response
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session
+  }
+
+  next()
+})
 
 conn
   .sync()
