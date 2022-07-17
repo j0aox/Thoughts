95d8db5814dabb721b8987a6044013e0ac9530d0
João Amador <nnettoolliveira@gmail.com>
2022-07-17 06:20:45 -0300

    :necktie: Middleware de autenticação de usuário

--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router()
 
 const ThoughtController = require('../controllers/ThoughtController')
 
+// helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
 router.get('/', ThoughtController.showThoughts)
+router.get('/dashboard', checkAuth, ThoughtController.dashboard)
 
 module.exports = router
