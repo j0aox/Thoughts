91518256fb910683b8794f88801185e4271023fd
João Amador <nnettoolliveira@gmail.com>
2022-07-18 14:42:37 -0300

    :necktie: Listando pensamentos

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -3,7 +3,13 @@ const User = require('../models/User')
 
 module.exports = class ThoughtController {
   static async showThoughts(req, res) {
-    res.render('thoughts/home')
+    const thoughtsData = await Thought.findAll({
+      include: User
+    })
+
+    const thoughts = thoughtsData.map((result) => result.get({ plain: true }))
+
+    res.render('thoughts/home', { thoughts })
   }
 
   static async dashboard(req, res) {
