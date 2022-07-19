23628560b658f09f7650dde2e81bf18ff9dd7504
João Amador <nnettoolliveira@gmail.com>
2022-07-19 11:54:17 -0300

    :necktie: Buscando por pensamentos

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -1,15 +1,41 @@
 const Thought = require('../models/Thought')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ThoughtController {
   static async showThoughts(req, res) {
+    let search = ''
+
+    if (req.query.search) {
+      search = req.query.search
+    }
+
+    let order = 'DESC'
+
+    if (req.query.order === 'old') {
+      order = 'ASC'
+    } else {
+      order = 'DESC'
+    }
+
     const thoughtsData = await Thought.findAll({
-      include: User
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` }
+      },
+      order: [['createdAt', order]]
     })
 
     const thoughts = thoughtsData.map((result) => result.get({ plain: true }))
 
-    res.render('thoughts/home', { thoughts })
+    let thoughtsQty = thoughts.length
+
+    if (thoughtsQty === 0) {
+      thoughtsQty = false
+    }
+
+    res.render('thoughts/home', { thoughts, search, thoughtsQty })
   }
 
   static async dashboard(req, res) {
